dca818252fc4fcc79b91a60c9c0a0584a9eeca01
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-01-07 23:01:56 -0300

    Move speed component working to attach in object

--- Assets/Scripts/MoveSpeed.cs
+++ Assets/Scripts/MoveSpeed.cs
@@ -1,11 +1,3 @@
 using Unity.Entities;
 
-public struct MoveSpeed : IComponentData
-{
-    // For create a dots component, we only need to define a struct that implements IComponentData
-    // We don't have to add functions or methods on components, just data
-    
-    public float value; // public fields are ok for ECS components
-    
-    
-}
+
--- Assets/Scripts/MoveSpeedAuthoring.cs
+++ Assets/Scripts/MoveSpeedAuthoring.cs
@@ -1,19 +1,35 @@
 using Unity.Entities;
 using UnityEngine;
 
+
+
+public struct MoveSpeed : IComponentData
+{
+    // For create a dots component, we only need to define a struct that implements IComponentData
+    // We don't have to add functions or methods on components, just data
+    
+    public float value; // public fields are ok for ECS components
+    
+    
+}
+
 public class MoveSpeedAuthoring : MonoBehaviour
 {
     public float value;
 
     public class Baker : Baker<MoveSpeedAuthoring>
     {
-        
         public override void Bake(MoveSpeedAuthoring authoring)
         {
+            Entity entity = GetEntity(TransformUsageFlags.Dynamic);
             
+            AddComponent(entity, new MoveSpeed
+            {
+                value = authoring.value
+            });
         }
-        
     }
-    
 }
+
+
 
