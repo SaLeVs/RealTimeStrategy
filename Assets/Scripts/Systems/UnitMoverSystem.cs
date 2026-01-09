fddcc375bdc3dcbc5fac1c07ca398d00b743afaf
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-01-08 21:59:55 -0300

    Moves units using system

--- Assets/Scripts/Systems/UnitMoverSystem.cs
+++ Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -1,5 +1,6 @@
 using Unity.Burst;
 using Unity.Entities;
+using Unity.Mathematics;
 using Unity.Transforms;
 
 // System as a suffix is a convention to indicate that this struct is a system
@@ -14,10 +15,16 @@ partial struct UnitMoverSystem : ISystem
     public void OnUpdate(ref SystemState state)
     {
         // RefRW is a class that allows us to convert a value type to a reference type
+        // RW stands for Read/Write
+        // RO stands for Read Only
+        // It's good to choice the RO when we don't need to write to the component for better performance
         
         foreach (RefRW<LocalTransform> localTransform in SystemAPI.Query<RefRW<LocalTransform>>())
         {
+            // Look that we use RW for modify and RO for read only
+            // DOTS have a special delta time that is different from UnityEngine.Time
             
+            localTransform.ValueRW.Position = localTransform.ValueRO.Position + new float3(1, 0, 0) * SystemAPI.Time.DeltaTime;
         }
     }
     
