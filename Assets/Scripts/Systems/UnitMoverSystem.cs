b1b321d45bdf93754a4ae0906d156e3d83bcb3e2
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-01-13 08:44:26 -0300

    Moving units using job system

--- Assets/Scripts/Systems/UnitMoverSystem.cs
+++ Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -16,32 +16,55 @@ partial struct UnitMoverSystem : ISystem
     [BurstCompile] // Burst compile attribute is for improving performance
     public void OnUpdate(ref SystemState state)
     {
+        UnitMoverJob unitMoverJob = new UnitMoverJob
+        {
+            deltaTime = SystemAPI.Time.DeltaTime
+        };
+
+        unitMoverJob.ScheduleParallel();
+
+        /*
         // RefRW is a class that allows us to convert a value type to a reference type
         // RW stands for Read/Write
         // RO stands for Read Only
         // It's good to choice the RO when we don't need to write to the component for better performance
-        
+
         foreach ((
-                     RefRW<LocalTransform> localTransform, 
+                     RefRW<LocalTransform> localTransform,
                      RefRO<UnitMover> unitMover,
-                     RefRW<PhysicsVelocity> physicsVelocity) 
+                     RefRW<PhysicsVelocity> physicsVelocity)
                         in SystemAPI.Query<
                            RefRW<LocalTransform>, RefRO<UnitMover>, RefRW<PhysicsVelocity>>())
         {
             // Look that we use RW for modify and RO for read only
-            // DOTS have a special delta time that is different from UnityEngine.Time
-            // Float3 means a vector with 3 float values (x, y, z)
-            
-            float3 moveDirection = unitMover.ValueRO.targetPosition - localTransform.ValueRO.Position;
-            
-            moveDirection = math.normalize(moveDirection);
-            
-            localTransform.ValueRW.Rotation = math.slerp(localTransform.ValueRO.Rotation, quaternion.LookRotation(moveDirection, math.up()), SystemAPI.Time.DeltaTime * unitMover.ValueRO.rotationSpeed); // Math.up() is (0, 1, 0)
 
-            physicsVelocity.ValueRW.Linear = moveDirection * unitMover.ValueRO.moveSpeed;
-            physicsVelocity.ValueRW.Angular = float3.zero; // We add this to avoid unwanted rotation
-            // localTransform.ValueRW.Position += moveDirection * moveSpeed.ValueRO.value * SystemAPI.Time.DeltaTime;
-        }
+        } */
+
     }
     
+}
+
+
+public partial struct UnitMoverJob : IJobEntity
+{
+    // In DOTS documentation, the recommended way to pass data to jobs is using in and ref keywords
+    // in is for read-only data
+    // ref is for read and write data
+    
+    public float deltaTime;
+    public void Execute(ref LocalTransform localTransform, in UnitMover unitMover, ref PhysicsVelocity physicsVelocity)
+    {
+        // DOTS have a special delta time that is different from UnityEngine.Time
+        // Float3 means a vector with 3 float values (x, y, z)
+            
+        float3 moveDirection = unitMover.targetPosition - localTransform.Position;
+            
+        moveDirection = math.normalize(moveDirection);
+            
+        localTransform.Rotation = math.slerp(localTransform.Rotation, quaternion.LookRotation(moveDirection, math.up()), deltaTime * unitMover.rotationSpeed); // Math.up() is (0, 1, 0)
+
+        physicsVelocity.Linear = moveDirection * unitMover.moveSpeed;
+        physicsVelocity.Angular = float3.zero; // We add this to avoid unwanted rotation
+        // localTransform.ValueRW.Position += moveDirection * moveSpeed.ValueRO.value * SystemAPI.Time.DeltaTime;
+    }
 }
