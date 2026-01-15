b5bed076a76bc5645e2e72486da1a36c1acb1e0e
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-01-14 23:36:27 -0300

    Adds a selectable component to soldiers

---
+++ Assets/Scripts/Authoring/SelectedAuthoring.cs
@@ -0,0 +1,25 @@
+using Unity.Entities;
+using UnityEngine;
+
+// In dots, we avoid structural changes as much as possible (Expensive operations)
+// For avoid to remove and add components frequently, we can use IEnableableComponent interface
+// This interface allow us to enable or disable a component without remove it from the entity
+public struct Selected : IComponentData, IEnableableComponent
+{
+       
+}
+
+public class SelectedAuthoring : MonoBehaviour
+{
+    public class SelectedAuthoringBaker : Baker<SelectedAuthoring>
+    {
+        public override void Bake(SelectedAuthoring authoring)
+        {
+            Entity entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent(entity, new Selected());
+        }
+    }
+}
+
+
+
