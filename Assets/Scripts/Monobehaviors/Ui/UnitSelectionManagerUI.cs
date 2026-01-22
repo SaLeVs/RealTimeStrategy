cffecc11236660a2bb42bb7d50a85ee9a778623c
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-01-21 22:39:07 -0300

    Corrects selection area scaling for different canvases

--- Assets/Scripts/Monobehaviors/Ui/UnitSelectionManagerUI.cs
+++ Assets/Scripts/Monobehaviors/Ui/UnitSelectionManagerUI.cs
@@ -6,6 +6,9 @@ public class UnitSelectionManagerUI : MonoBehaviour
     [SerializeField] private RectTransform selectionAreaRectTransform;
     [SerializeField] private UnitSelectionManager unitSelectionManager;
     
+    [SerializeField] private Canvas canvas;
+    
+    
     private void Awake()
     {
         unitSelectionManager.OnSelectionStart += UnitSelectionManager_OnSelectionStart;
@@ -24,7 +27,6 @@ public class UnitSelectionManagerUI : MonoBehaviour
     private void UnitSelectionManager_OnSelectionEnd()
     {
         selectionAreaRectTransform.gameObject.SetActive(false);
-        
     }
 
     private void Update()
@@ -39,8 +41,10 @@ public class UnitSelectionManagerUI : MonoBehaviour
     {
         Rect selectionAreaRect = unitSelectionManager.GetSelectionAreaRect();
         
-        selectionAreaRectTransform.anchoredPosition = new Vector2(selectionAreaRect.x, selectionAreaRect.y);
-        selectionAreaRectTransform.sizeDelta = new Vector2(selectionAreaRect.width, selectionAreaRect.height);
+        float canvasScale = canvas.transform.localScale.x;
+        
+        selectionAreaRectTransform.anchoredPosition = new Vector2(selectionAreaRect.x, selectionAreaRect.y ) / canvasScale;
+        selectionAreaRectTransform.sizeDelta = new Vector2(selectionAreaRect.width, selectionAreaRect.height) / canvasScale;
     }
     
 }
