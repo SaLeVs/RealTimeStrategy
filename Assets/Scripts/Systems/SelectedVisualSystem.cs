7ab894599c24411ad5335f4a97d45000952997a9
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-01-30 07:02:57 -0300

    Using events for change visual system

--- Assets/Scripts/Systems/SelectedVisualSystem.cs
+++ Assets/Scripts/Systems/SelectedVisualSystem.cs
@@ -7,16 +7,19 @@ partial struct SelectedVisualSystem : ISystem
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach (RefRO<Selected> selected in SystemAPI.Query<RefRO<Selected>>().WithDisabled<Selected>())
+        foreach (RefRO<Selected> selected in SystemAPI.Query<RefRO<Selected>>().WithPresent<Selected>())
         {
-            RefRW<LocalTransform> visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.selectedVisualEntity);
-            visualLocalTransform.ValueRW.Scale = 0f;
-        }
-        
-        foreach (RefRO<Selected> selected in SystemAPI.Query<RefRO<Selected>>())
-        {
-            RefRW<LocalTransform> visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.selectedVisualEntity);
-            visualLocalTransform.ValueRW.Scale = selected.ValueRO.showVisualScale;
+            if (selected.ValueRO.onSelected)
+            {
+                RefRW<LocalTransform> visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.selectedVisualEntity);
+                visualLocalTransform.ValueRW.Scale = selected.ValueRO.showVisualScale;
+            }
+            
+            if (selected.ValueRO.onDeselected)
+            {
+                RefRW<LocalTransform> visualLocalTransform = SystemAPI.GetComponentRW<LocalTransform>(selected.ValueRO.selectedVisualEntity);
+                visualLocalTransform.ValueRW.Scale = 0f;
+            }
         }
     }
 }
