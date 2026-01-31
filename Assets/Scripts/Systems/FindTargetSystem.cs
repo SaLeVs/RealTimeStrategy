252f1f07e83bcd5cd5b970053d48fb5b2da2fe24
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-01-31 19:41:16 -0300

    Store target on units

--- Assets/Scripts/Systems/FindTargetSystem.cs
+++ Assets/Scripts/Systems/FindTargetSystem.cs
@@ -16,10 +16,8 @@ partial struct FindTargetSystem : ISystem
         
         NativeList<DistanceHit> distanceHits = new NativeList<DistanceHit>(Allocator.Temp);
         
-        
-        
-        foreach ((RefRO<LocalTransform> localTransform, RefRO<FindTarget> findTarget) 
-                 in SystemAPI.Query<RefRO<LocalTransform>, RefRO<FindTarget>>())
+        foreach ((RefRO<LocalTransform> localTransform, RefRO<FindTarget> findTarget, RefRW<Target> target) 
+                 in SystemAPI.Query<RefRO<LocalTransform>, RefRO<FindTarget>, RefRW<Target>>())
         {
             distanceHits.Clear();
             CollisionFilter collisionFilter = new CollisionFilter
@@ -28,7 +26,6 @@ partial struct FindTargetSystem : ISystem
                 CollidesWith = 1u << GameAssets.UNITS_LAYER, // We put 1u << 6 because our units are in layer 6, and we need to shift 1 to the left 6 times to get the correct bitmask
                 GroupIndex = 0
             };
-            Debug.Log($"-");
             if (collisionWorld.OverlapSphere(localTransform.ValueRO.Position, findTarget.ValueRO.range,
                     ref distanceHits,
                     collisionFilter))
@@ -39,7 +36,7 @@ partial struct FindTargetSystem : ISystem
                     if (targetUnit.faction == findTarget.ValueRO.targetFaction)
                     {
                         // Valid target found
-                        Debug.Log($"Target found: {distanceHit.Entity}");
+                        target.ValueRW.targetEntity = distanceHit.Entity;
                     }
                 }
             }
