1d873206ecd4c8628463362c5d999f07bfadb029
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-02-02 06:17:22 -0300

    Add shoot logic

---
+++ Assets/Scripts/Systems/ShootAttackSystem.cs
@@ -0,0 +1,31 @@
+using Unity.Burst;
+using Unity.Entities;
+using UnityEngine;
+
+partial struct ShootAttackSystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        foreach ((RefRW<ShootAttack> shootAttack, RefRO<Target> target) in 
+                 SystemAPI.Query<RefRW<ShootAttack>, RefRO<Target>>())
+        {
+            if (target.ValueRO.targetEntity == Entity.Null) // If Entity.null is true, we don't have a target
+            {
+                continue;
+            }
+            
+            shootAttack.ValueRW.timer -= SystemAPI.Time.DeltaTime;
+            
+            if (shootAttack.ValueRO.timer > 0f)
+            {
+                continue;
+            }
+            
+            shootAttack.ValueRW.timer = shootAttack.ValueRO.timerMax;
+            Debug.Log("Shoot");
+        }
+
+        
+    }
+}
