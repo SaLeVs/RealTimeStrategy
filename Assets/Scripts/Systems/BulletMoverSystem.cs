f98fcd555bd3ac2c6c5ef2c70feadfb5eedd415e
SaLeVs <alvesanicetodasilvag@gmail.com>
2026-02-06 06:43:02 -0300

    Correct system for handle when bullet overshot player

--- Assets/Scripts/Systems/BulletMoverSystem.cs
+++ Assets/Scripts/Systems/BulletMoverSystem.cs
@@ -16,11 +16,22 @@ partial struct BulletMoverSystem : ISystem
                  in SystemAPI.Query<RefRW<LocalTransform>, RefRO<Bullet>, RefRO<Target>>().WithEntityAccess())
         {
             LocalTransform targetLocalTransform = SystemAPI.GetComponent<LocalTransform>(target.ValueRO.targetEntity);
+            
+            float distanceBeforeShooting = math.distancesq(localTransform.ValueRW.Position, targetLocalTransform.Position);
+            
             float3 moveDirection = targetLocalTransform.Position - localTransform.ValueRO.Position;
             moveDirection =  math.normalize(moveDirection);
             
             localTransform.ValueRW.Position += moveDirection * bullet.ValueRO.speed * SystemAPI.Time.DeltaTime;
 
+            float distanceAfterShooting = math.distancesq(localTransform.ValueRW.Position, targetLocalTransform.Position);
+            
+            if(distanceAfterShooting > distanceBeforeShooting)
+            {
+                // overshot the target, so we set the position to the target's position
+                localTransform.ValueRW.Position = targetLocalTransform.Position;
+            }
+            
             float destroyDistance = 0.2f;
             
             if (math.distancesq(localTransform.ValueRW.Position, targetLocalTransform.Position) < destroyDistance)
